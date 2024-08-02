4268c0b6997061bad90a973dbab7a80b39664f99
Abigail Casabuena <Abigail.Casabuena@polaris.com>
2024-08-02 22:42:34 +0800

    string check file

---
+++ app/src/main/java/com/example/coverallsandroid/StringCheck.kt
@@ -0,0 +1,29 @@
+package com.example.coverallsandroid
+
+class StringCheck {
+
+    fun reverseString(input: String): String {
+        return input.reversed()
+    }
+
+    fun isPalindrome(input: String): Boolean {
+        return input == input.reversed()
+    }
+
+    fun capitalizeFirstLetter(input: String): String {
+        return if (input.isNotEmpty()) {
+            input[0].uppercaseChar() + input.substring(1)
+        } else {
+            input
+        }
+    }
+
+    fun countVowels(input: String): Int {
+        val vowels = "aeiouAEIOU"
+        return input.count { it in vowels }
+    }
+
+    fun removeWhitespace(input: String): String {
+        return input.replace(" ", "")
+    }
+}
