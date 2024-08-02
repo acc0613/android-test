b0d367138b260cc7175439b578c2881293b99088
Abigail Casabuena <Abigail.Casabuena@polaris.com>
2024-08-02 23:09:31 +0800

    unit test check

---
+++ app/src/test/java/com/example/coverallsandroid/StringCheckTest.kt
@@ -0,0 +1,27 @@
+package com.example.coverallsandroid
+
+import com.example.coverallsandroid.StringCheck
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class StringCheckTest {
+    private val stringUtils = StringCheck()
+
+    @Test
+    fun reverseString_simpleString_reversedCorrectly() {
+        val result = stringUtils.reverseString("hello")
+        assertEquals("olleh", result)
+    }
+
+    @Test
+    fun isPalindrome_palindrome_returnsTrue() {
+        val result = stringUtils.isPalindrome("madam")
+        assertEquals(true, result)
+    }
+
+    @Test
+    fun capitalizeFirstLetter_emptyString_returnsEmptyString() {
+        val result = stringUtils.capitalizeFirstLetter("")
+        assertEquals("", result)
+    }
+}
